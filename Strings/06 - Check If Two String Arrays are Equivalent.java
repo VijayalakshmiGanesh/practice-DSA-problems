4de5fcda2f520c1a9a321b164e45b804309f3a48
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-02-08 20:20:27 +0530

    Qn.6 in Strings done

---
+++ Strings/06 - Check If Two String Arrays are Equivalent.java
@@ -0,0 +1,26 @@
+package Strings;
+
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+
+        StringBuilder w1 = new StringBuilder();
+        StringBuilder w2 = new StringBuilder();
+
+        for (String word : word1) {
+            w1.append(word);
+        }
+
+        for (String word : word2) {
+            w2.append(word);
+        }
+
+        return w1.toString().equals(w2.toString());
+    }
+}
+// Another way
+// class Solution {
+// public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+// if (String.join("", word1).equals(String.join("", word2))) return true;
+// return false;
+// }
+// }
