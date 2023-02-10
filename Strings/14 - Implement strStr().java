1a6c68a8b8b418d8ec7dd716968fd00117f3c52f
VijayalakshmiGanesh <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-02-10 10:40:59 +0530

    Addded better way for Qn.14 in String

--- Strings/14 - Implement strStr().java
+++ Strings/14 - Implement strStr().java
@@ -27,4 +27,16 @@ class Solution {
 
         return idx;
     }
+
 }
+
+    // Better way
+public int strStr(String haystack, String needle) {
+    for (int i = 0; ; i++) { // The length of haystack
+        for (int j = 0; ; j++) { // The length of needle
+          if (j == needle.length()) return i; // If at this point we have navigated through the entire length of needle, we have found a solution, haystack[i].
+          if (i + j == haystack.length()) return -1; // This happens when we run out of elements in haystack, but there are still elements in needle. 
+          if (needle.charAt(j) != haystack.charAt(i + j)) break; // We stop comparing after needle[j], so i will be incremented and cycle repeats itself. 
+        }
+      }
+    }
